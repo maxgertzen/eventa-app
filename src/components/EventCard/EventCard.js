a25b202d67c19342e9ecb1d49b580043135292ba
maxgertzen <maxgertzen@gmail.com>
2021-07-10 17:24:51 +0300

    added event card

---
+++ src/components/EventCard/EventCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.article`
+    height: 10rem;
+    width: 15rem
+`
+
+const EventCard = ({ event }) => {
+    return (
+        <Card>
+
+        </Card>
+    )
+}
--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { getEvents } from '../api';
 import { useHttp } from '../hooks/useHttp';
 
-const ExplorePage = () => {
+import styled from 'styled-components';
 
+const Page = styled.section`
+    width: 100%;
+`
+
+const ExplorePage = () => {
+    const {
+        response: events,
+        error,
+        loading
+    } = useHttp(getEvents);
+
+    return (
+        <Page />
+    )
 }
